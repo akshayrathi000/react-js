54a7568c99685fb771bcc643f03d9e1e3f438279
Akshay Kumar <73302530+akshayrathi000@users.noreply.github.com>
2022-06-29 11:05:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <code> neww </code> 
+           <code> websitee </code> 
         </p>
         
       </header>
